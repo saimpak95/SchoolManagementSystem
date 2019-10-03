293f0b1c132150bffaa76085e3ed65f9a5fd64ac
Saim Siddiqui <Saimpak95@hotmail.com>
2019-10-03 11:12:18 +0500

    DONE WITH SHIFT FORM

--- SchoolManagementSystem/SHIFT.Designer.cs
+++ SchoolManagementSystem/SHIFT.Designer.cs
@@ -28,9 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.ErrorShiftFrom = new System.Windows.Forms.Label();
+            this.ErrorShiftName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TXTShiftName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TXTShiftFrom = new System.Windows.Forms.DateTimePicker();
+            this.TXTShiftTo = new System.Windows.Forms.DateTimePicker();
+            this.ErrorShiftTo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SnoGv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GVShiftID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GVShiftName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GVShiftFrom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GvShiftTo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel7
@@ -43,7 +65,25 @@
             // 
             // panel6
             // 
+            this.panel6.Controls.Add(this.TXTShiftTo);
+            this.panel6.Controls.Add(this.ErrorShiftTo);
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Controls.Add(this.TXTShiftFrom);
+            this.panel6.Controls.Add(this.ErrorShiftFrom);
+            this.panel6.Controls.Add(this.ErrorShiftName);
+            this.panel6.Controls.Add(this.label1);
+            this.panel6.Controls.Add(this.label2);
+            this.panel6.Controls.Add(this.TXTShiftName);
             this.panel6.Size = new System.Drawing.Size(234, 571);
+            this.panel6.Controls.SetChildIndex(this.TXTShiftName, 0);
+            this.panel6.Controls.SetChildIndex(this.label2, 0);
+            this.panel6.Controls.SetChildIndex(this.label1, 0);
+            this.panel6.Controls.SetChildIndex(this.ErrorShiftName, 0);
+            this.panel6.Controls.SetChildIndex(this.ErrorShiftFrom, 0);
+            this.panel6.Controls.SetChildIndex(this.TXTShiftFrom, 0);
+            this.panel6.Controls.SetChildIndex(this.label4, 0);
+            this.panel6.Controls.SetChildIndex(this.ErrorShiftTo, 0);
+            this.panel6.Controls.SetChildIndex(this.TXTShiftTo, 0);
             // 
             // panel2
             // 
@@ -51,11 +91,190 @@
             // 
             // panel5
             // 
+            this.panel5.Controls.Add(this.groupBox2);
             this.panel5.Size = new System.Drawing.Size(908, 571);
+            this.panel5.Controls.SetChildIndex(this.panel7, 0);
+            this.panel5.Controls.SetChildIndex(this.groupBox2, 0);
             // 
             // panel4
             // 
             this.panel4.Size = new System.Drawing.Size(908, 40);
+            // 
+            // ErrorShiftFrom
+            // 
+            this.ErrorShiftFrom.AutoSize = true;
+            this.ErrorShiftFrom.Location = new System.Drawing.Point(211, 131);
+            this.ErrorShiftFrom.Name = "ErrorShiftFrom";
+            this.ErrorShiftFrom.Size = new System.Drawing.Size(12, 15);
+            this.ErrorShiftFrom.TabIndex = 28;
+            this.ErrorShiftFrom.Text = "*";
+            this.ErrorShiftFrom.Visible = false;
+            // 
+            // ErrorShiftName
+            // 
+            this.ErrorShiftName.AutoSize = true;
+            this.ErrorShiftName.Location = new System.Drawing.Point(211, 87);
+            this.ErrorShiftName.Name = "ErrorShiftName";
+            this.ErrorShiftName.Size = new System.Drawing.Size(12, 15);
+            this.ErrorShiftName.TabIndex = 27;
+            this.ErrorShiftName.Text = "*";
+            this.ErrorShiftName.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "SHIFT FROM ";
+            // 
+            // TXTShiftName
+            // 
+            this.TXTShiftName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.TXTShiftName.Enabled = false;
+            this.TXTShiftName.Location = new System.Drawing.Point(12, 105);
+            this.TXTShiftName.Name = "TXTShiftName";
+            this.TXTShiftName.Size = new System.Drawing.Size(211, 23);
+            this.TXTShiftName.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "SHIFT NAME";
+            // 
+            // TXTShiftFrom
+            // 
+            this.TXTShiftFrom.Enabled = false;
+            this.TXTShiftFrom.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.TXTShiftFrom.Location = new System.Drawing.Point(15, 150);
+            this.TXTShiftFrom.Name = "TXTShiftFrom";
+            this.TXTShiftFrom.Size = new System.Drawing.Size(208, 23);
+            this.TXTShiftFrom.TabIndex = 29;
+            // 
+            // TXTShiftTo
+            // 
+            this.TXTShiftTo.Enabled = false;
+            this.TXTShiftTo.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.TXTShiftTo.Location = new System.Drawing.Point(15, 198);
+            this.TXTShiftTo.Name = "TXTShiftTo";
+            this.TXTShiftTo.Size = new System.Drawing.Size(208, 23);
+            this.TXTShiftTo.TabIndex = 32;
+            // 
+            // ErrorShiftTo
+            // 
+            this.ErrorShiftTo.AutoSize = true;
+            this.ErrorShiftTo.Location = new System.Drawing.Point(211, 179);
+            this.ErrorShiftTo.Name = "ErrorShiftTo";
+            this.ErrorShiftTo.Size = new System.Drawing.Size(12, 15);
+            this.ErrorShiftTo.TabIndex = 31;
+            this.ErrorShiftTo.Text = "*";
+            this.ErrorShiftTo.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 15);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "SHIFT FROM ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(908, 520);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DETAILS";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SnoGv,
+            this.GVShiftID,
+            this.GVShiftName,
+            this.GVShiftFrom,
+            this.GvShiftTo});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(902, 498);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // SnoGv
+            // 
+            this.SnoGv.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.SnoGv.HeaderText = "SNO";
+            this.SnoGv.Name = "SnoGv";
+            this.SnoGv.ReadOnly = true;
+            this.SnoGv.Width = 56;
+            // 
+            // GVShiftID
+            // 
+            this.GVShiftID.HeaderText = "ID";
+            this.GVShiftID.Name = "GVShiftID";
+            this.GVShiftID.ReadOnly = true;
+            this.GVShiftID.Visible = false;
+            // 
+            // GVShiftName
+            // 
+            this.GVShiftName.HeaderText = "SHIFT NAME";
+            this.GVShiftName.Name = "GVShiftName";
+            this.GVShiftName.ReadOnly = true;
+            // 
+            // GVShiftFrom
+            // 
+            this.GVShiftFrom.HeaderText = "SHIFT FROM";
+            this.GVShiftFrom.Name = "GVShiftFrom";
+            this.GVShiftFrom.ReadOnly = true;
+            // 
+            // GvShiftTo
+            // 
+            this.GvShiftTo.HeaderText = "SHIFT TO";
+            this.GvShiftTo.Name = "GvShiftTo";
+            this.GvShiftTo.ReadOnly = true;
             // 
             // SHIFT
             // 
@@ -65,12 +284,33 @@
             this.Name = "SHIFT";
             this.Text = "SHIFT";
             this.panel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker TXTShiftTo;
+        private System.Windows.Forms.Label ErrorShiftTo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker TXTShiftFrom;
+        private System.Windows.Forms.Label ErrorShiftFrom;
+        private System.Windows.Forms.Label ErrorShiftName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TXTShiftName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SnoGv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GVShiftID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GVShiftName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GVShiftFrom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GvShiftTo;
     }
 }
--- SchoolManagementSystem/SHIFT.cs
+++ SchoolManagementSystem/SHIFT.cs
@@ -16,5 +16,83 @@ namespace SchoolManagementSystem
         {
             InitializeComponent();
         }
+
+
+        int edit = 0;
+        int ID = 0;
+        public override void AddBTN_Click(object sender, EventArgs e)
+        {
+            MainClass.Enable_Reset(panel6);
+            edit = 0;
+        }
+
+        public override void EditBTN_Click(object sender, EventArgs e)
+        {
+            MainClass.Enable(panel6);
+        }
+
+        public override void SaveBTN_Click(object sender, EventArgs e)
+        {
+            if (edit == 0)
+            {
+                Insertion.SHIFT(TXTShiftName.Text, TXTShiftFrom.Text, TXTShiftTo.Text);
+            }
+            if (edit == 1)
+            {
+                Updation.SHIFT(ID, TXTShiftName.Text, TXTShiftFrom.Text, TXTShiftTo.Text);
+
+            }
+            Retrieval.SHIFT(dataGridView1, GVShiftID, GVShiftName, GVShiftFrom, GvShiftTo);
+            MainClass.Disable_Reset(panel6);
+        }
+
+        public override void DeleteBTN_Click(object sender, EventArgs e)
+        {
+            if (edit == 1)
+            {
+
+                Deletion.SHIFT(ID);
+
+            }
+            Retrieval.SHIFT(dataGridView1, GVShiftID, GVShiftName, GVShiftFrom, GvShiftTo);
+            MainClass.Disable_Reset(panel6);
+        }
+
+        public override void ViewBTN_Click(object sender, EventArgs e)
+        {
+            Retrieval.SHIFT(dataGridView1, GVShiftID, GVShiftName, GVShiftFrom, GvShiftTo);
+        }
+
+        public override void SearchTXT_TextChanged(object sender, EventArgs e)
+        {
+            if (SearchTXT.Text != "")
+            {
+                Retrieval.SHIFT(dataGridView1, GVShiftID, GVShiftName, GVShiftFrom, GvShiftTo,SearchTXT.Text);
+            }
+            else
+                Retrieval.SHIFT(dataGridView1, GVShiftID, GVShiftName, GVShiftFrom, GvShiftTo);
+        }
+
+ 
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+          
+        }
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                ID = Convert.ToInt32(row.Cells["GVShiftID"].Value.ToString());
+                TXTShiftName.Text = row.Cells["GVShiftName"].Value.ToString();
+                TXTShiftFrom.Text = row.Cells["GVShiftFrom"].Value.ToString();
+                TXTShiftTo.Text = row.Cells["GvShiftTo"].Value.ToString();
+                MainClass.Disable(panel6);
+                edit = 1;
+            }
+        }
     }
 }
