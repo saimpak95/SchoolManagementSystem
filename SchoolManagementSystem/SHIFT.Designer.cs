16bf42a641ec7aaceedf2356fb1fc6e5eb3cf96a
Saim Siddiqui <Saimpak95@hotmail.com>
2019-10-03 09:09:07 +0500

    ADDED SHIFT WINDOW

---
+++ SchoolManagementSystem/SHIFT.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SchoolManagementSystem
+{
+    partial class SHIFT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel7
+            // 
+            this.panel7.Size = new System.Drawing.Size(908, 51);
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(234, 611);
+            // 
+            // panel6
+            // 
+            this.panel6.Size = new System.Drawing.Size(234, 571);
+            // 
+            // panel2
+            // 
+            this.panel2.Size = new System.Drawing.Size(908, 611);
+            // 
+            // panel5
+            // 
+            this.panel5.Size = new System.Drawing.Size(908, 571);
+            // 
+            // panel4
+            // 
+            this.panel4.Size = new System.Drawing.Size(908, 40);
+            // 
+            // SHIFT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 611);
+            this.Name = "SHIFT";
+            this.Text = "SHIFT";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
